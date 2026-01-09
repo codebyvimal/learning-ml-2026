f95e28cad3f105e5bdfe85530df218e333fe1db3
codebyvimal <vimworks.ai@gmail.com>
2026-01-10 00:19:26 +0530

    Practice 40 mins

--- month_01/Playground/Week-1/new.py
+++ month_01/Playground/Week-1/new.py
@@ -1,4 +1,10 @@
-name = "vimal\n"
-print(name)
-name = name.rstrip()
-print(name)
+students = []
+with open("names.csv") as file:
+    for line in file:
+        name,place = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["place"] = place
+        students.append(student)
+for student in students:
+    print(f"{student['name']} is in {student['place']}")
